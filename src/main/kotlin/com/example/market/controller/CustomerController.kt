39bdcde03fb76b91d64cf6557d54760a9eaced51
ehdgnl5249 <ehdgnl5249@naver.com>
2022-02-02 16:10:49 +0900

    ADD: data class 사용

--- src/main/kotlin/com/example/market/controller/CustomerController.kt
+++ src/main/kotlin/com/example/market/controller/CustomerController.kt
@@ -1,15 +1,16 @@
 package com.example.market.controller
 
+import com.example.market.entity.Customer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping
+@RequestMapping("customers")
 class CustomerController {
 
-    @GetMapping
-    fun helloWorld(): String {
-        return "Hello World"
+    @GetMapping("1")
+    fun getCustomer(): Customer {
+        return Customer("1", "test", "test@test.com")
     }
 }
---
+++ src/main/kotlin/com/example/market/entity/Customer.kt
@@ -0,0 +1,7 @@
+package com.example.market.entity
+
+data class Customer(
+    var id: String,
+    var name: String,
+    var email: String
+)
